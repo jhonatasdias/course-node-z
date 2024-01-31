8015ef65882ea50240868b57b7bdb56ac1cf292f
unknown <engjhonatadias@gmail.com>
2024-01-31 08:51:36 -0300

    Chapter 7

--- 7 - Web Servers/READEME.md
+++ 7 - Web Servers/READEME.md
@@ -50,3 +50,7 @@ Successful responses (200 – 299)
 Redirection messages (300 – 399)
 Client error responses (400 – 499)
 Server error responses (500 – 599)
+
+## What's Origin
+
+![Origin URL](/images/origin-url.jpg)
--- 7 - Web Servers/index.js
+++ 7 - Web Servers/index.js
@@ -1,10 +1,71 @@
 const http = require('http');
 
+const PORT = 3000;
+
+const friends = [
+    {
+        id: 0,
+        name: "Sir. Nikola Tesla"
+    },
+    {
+        id: 1,
+        name: "Sir. Isaac Newton"
+    },
+    {
+        id: 2,
+        name: "Sir. Albert Einstein"
+    }
+]
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
+    /* res.writeHead(200, {
         'Content-Type': 'text/plain',
     })
-    req.end('Hello! Sir.')
+    res.end('Hello! Sir.') */
+
+    const items = req.url.split('/');
+
+    console.log(items)
+
+    if ( items[1] === 'friend') {
+
+        if (items.length === 3) {
+
+            const friendIndex = +items[2]; // or Number(items[2])
+
+            console.log(friends[friendIndex])
+
+            res.setHeader('Content-Type', 'application/json');
+
+            res.write(JSON.stringify(friends[friendIndex]));
+
+            res.end();
+
+        } else {
+
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            })
+            res.end(JSON.stringify(friends))
+
+        }
+
+
+    } else if ( items[1] === 'message') {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<li>Node.js Server');
+        res.write('</html>');
+        res.end();
+
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+
 })
 
-server.listen();
+server.listen(PORT, () => {
+    console.log(`Listening on port http://localhost:${PORT}...`);
+});
