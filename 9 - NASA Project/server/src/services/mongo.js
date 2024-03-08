b661b004636aa269c380bb9e925e051583d4fe7e
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2024-03-07 22:10:44 -0300

    Mongo_URL add

--- 9 - NASA Project/server/src/services/mongo.js
+++ 9 - NASA Project/server/src/services/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // const MONGO_URL = process.env.MONGO_URL;
 
 const mongoConfig = require('../../../config');
-const MONGO_URL = mongoConfig.MONGO_URL;
+const MONGO_URL = "mongodb+srv://brufesdias:dw9wPWyJLAQh9XAo@cluster-nasa-project.zo4guy3.mongodb.net/nasa?retryWrites=true&w=majority";
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
